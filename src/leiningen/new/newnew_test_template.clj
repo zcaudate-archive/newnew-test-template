5e02de963d91ba4b424863988f19b62643957bbc
zcaudate <z@caudate.me>
2012-10-23 12:02:18 +1100

    Added examples of directives

--- src/leiningen/new/newnew_test_template.clj
+++ src/leiningen/new/newnew_test_template.clj
@@ -1,12 +1,12 @@
 (ns leiningen.new.newnew-test-template
-  (:use [leiningen.newnew.templates :only [deftemplate]]
-        [clojure.java.io :only [input-stream]])
+  (:use [clojure.java.io :only [input-stream]])
   (:require [leiningen.newnew.templates :as t]))
 
-(deftemplate newnew-test-template
+(defn newnew-test-template
   "An example template for websites"
   [name]
-  {:data 
+  {:template true
+   :data 
     {:raw-name name
      :name (t/project-name name)
      :namespace (t/sanitize-ns name)
@@ -14,25 +14,23 @@
      :year (t/year)}
 
    :directives ;; key, input/output or [["input" "output"]] 
-     {:new-dirs     ["scratch"
-                     "example/new-dirs/dir1" 
-                     "example/new-dirs/dir2"]
-      :copy-dirs    ["resource"
-                     ["example/dir" "example/copy-dirs/dir"]
-                     "example/{{name}}"
-                     ["example/copy-dirs/{{name}}" "example/copy-dirs/{{name}}/copy"]]
-      :render-dirs  ["src"
+     {:render-dirs  [["" :except ["src" "example" "resources"]]
+                     ["src"]
                      ["example/dir" "example/render-dirs/dir"]
-                     "example/{{name}}"
                      ["example/render-dirs/{{name}}" "example/render-dirs/{{name}}/rendered"]]
-      :copy-files   ["example/copy-files/file.txt" 
+      :copy-dirs    [["resources"]
+                     ["example/dir" "example/copy-dirs/dir"]
+                     ["example/copy-dirs/{{name}}" "example/copy-dirs/{{name}}/copy"]]
+      :copy-files   [["example/copy-files/file.txt"] 
                      ["example/copy-files/file.txt" "example/copy-files/copy.txt"]
-                     "example/copy-files/{{name}}/file.txt"
-                     ["example/copy-files/{{name}}/file.txt" "example/copy-files/{{name}}/copy.txt"]]
-      :render-files ["example/render-files/{{name}}/file.txt"
-                     ["example/render-files/{{name}}/file.txt" 
-                      "example/render-files/{{name}}/render.txt"]]
-      :create-files ["example/create-files/{{name}}/empty.txt"
-                    [(input-stream "http://www.google.com") "example/create-files/{{name}}/inputstream.txt"]
-                    ["hello {{name}}"  "example/create-files/{{name}}/string.txt"]]
-     }})
+                     ["example/copy-files/{{name}}.txt" "example/copy-files/{{name}}-copy.txt"]]
+      :render-files [["example/render-files/{{name}}.txt"]
+                     ["example/render-files/{{name}}.txt"] 
+                     ["example/render-files/render.txt"]]
+      :new-dirs     [["scratch"]
+                     ["example/new-dirs/dir1"] 
+                     ["example/new-dirs/dir2"]]
+      :create-files [["example/create-files/{{name}}/empty.txt"]
+                     [(input-stream "http://www.google.com") "example/create-files/{{name}}/inputstream.txt"]
+                     ["hello {{name}} is not rendered"  "example/create-files/{{name}}/string.txt"]]}
+})
